0b8fdb40a9e155d869e067742393cb4356bfa9e5
Fabiel Leon <fabiel.leon.oliva@gmail.com>
2021-03-10 21:06:16 -0500

    exit if error

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,33 @@ echo "NODE version: $(node -v)"
 
 echo "::group:: Installing"
 npm ci
+if [ $? -eq 0 ]
+then
+  echo "The script ran ok"
+else
+  echo "The installation has failed" >&2
+  exit 1
+fi
 echo "::endgroup::"
 
 echo "::group:: Building"
 npm run build
+if [ $? -eq 0 ]
+then
+  echo "The script ran ok"
+else
+  echo "The build has failed" >&2
+  exit 1
+fi
 echo "::endgroup::"
 
 echo "::group:: Installing Production"
 npm ci --production
+if [ $? -eq 0 ]
+then
+  echo "The script ran ok"
+else
+  echo "The installation has failed" >&2
+  exit 1
+fi
 echo "::endgroup::"
